c1eb38d7f78b4c6ad922910151188febe14fce8c
Joonhee Jeong <jeonggoo75@gmail.com>
2020-10-07 15:13:21 +0900

    feat: add function for converting xml string to object

--- fetch_evc_list.ts
+++ fetch_evc_list.ts
@@ -1,4 +1,52 @@
-function xmlStrToObj(str: string): object {}
+interface LooseObject {
+    [key: string]: any
+}
+
+function xmlStrToObj(str: string): object {
+    let items: LooseObject[] = [];
+    let item: LooseObject = {};
+    let i = str.indexOf("<item>");
+    let beg: number;
+    let end: number;
+    let tagName: string = "dummy";
+L1:
+    while (true) {
+        if (str[i] === "<") {
+            beg = i+1;
+            end = str.indexOf(">", beg);
+            i = end+1;
+            tagName = str.substring(beg, end);
+            switch (tagName) {
+                case "/items":
+                    break L1;
+                case "item":
+                    item = {};
+                    break;
+                case "/item":
+                    items.push(item);
+                    break;
+                defaut:
+                    break;
+            }
+        } else {
+            beg = i;
+            end = str.indexOf("<", beg);
+            i = str.indexOf("<", end+1);
+            const text: string = str.substring(beg, end);
+            let val: any;
+            switch (tagName) {
+                case "lat": case "lng": case "longi":
+                    val = parseFloat(text);
+                    break;
+                default:
+                    val = text;
+                    break;
+            }
+            item[tagName] = val;
+        }
+    }
+    return items;
+}
 
 async function fetchXml(url: string): Promise<object> {
     let res =  await fetch(url);
@@ -28,3 +76,9 @@ function fetchXmlAndMakeJson(url: string, path: string): void {
         console.log(`Fetch attempt for "${url}" finished.`);
     });
 }
+
+const xmlStr = Deno.readTextFile("./test_evc_list.xml");
+xmlStr.then(str => {
+    console.log(str);
+    console.log(xmlStrToObj(str));
+});
